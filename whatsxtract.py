50cf4fee8e24b0aca2cf173307b4b5eb35f4cb2a
Maik Costa <maikcosta@gmail.com>
2021-01-13 14:36:01 -0300

    Extract students and create in and out

--- whatsxtract.py
+++ whatsxtract.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from glob import glob
 import os
 import re
@@ -6,7 +7,7 @@ class Grupo:
     def __init__(self, arquivo):
         self.arquivo = arquivo
         self.nome, self.numero = self.extrair_nome_numero()
-        self.alunos = '' #Alunos.extrair_alunos
+        self.alunos = Aluno.extrair_alunos(self.arquivo)
 
     def extrair_nome_numero(self):
          padrao_grupo = r"Conversa do WhatsApp com (\[(\d{1,})].*)"
@@ -23,6 +24,65 @@ class Grupo:
 
     def remover_duplicados():
         ...
+
+class Operacao(Enum):
+    ENTRADA = 1
+    SAIDA = 2
+    MUDANCA = 3
+
+class Aluno:
+    def __init__(self, numero):
+        self.numero = numero
+        self.presente = True
+    
+    def extrair_alunos(arquivo):
+        padrao_aluno = r"([\d/ :]*) - *(\+55 [\d\s-]*) (entrou|saiu)(\+55 [\d\s-]*$)?"
+        alunos = []
+        with open(arquivo, encoding='UTF-8') as arq:
+            conversa = arq.readlines()
+
+        for linha in conversa:
+            resultado = re.search(padrao_aluno,linha,re.MULTILINE)
+
+            if resulta != None:
+                horario = resultado.group(1)
+                numero = resultado.group(2)
+                operacao = resultado.group(3)
+                novo_numero =resultado.group(4)
+
+                print("||", horario,numero,operacao, novo_numero)
+
+                if novo_numero is not None:
+                    for aluno in alunos:
+                        if aluno.numero == numero:
+                            aluno.alterar_numero(novo_numero)
+                elif 'entrou' in operacao:
+                    aluno = Aluno.criar_aluno(numero)
+                    aluno.entrada_saida(Operacao.ENTRADA, horario)
+                    alunos.append(aluno)
+                else
+                    for aluno in alunos:
+                        if aluno.numero == numero:
+                            aluno.entrada_saida(Operacao.SAIDA, horario)               
+
+
+
+
+    def alterar_numero(self, novo_numero):
+        self.numero = novo_numero
+    def criar_aluno(numero):
+        return Aluno(numero)
+
+    def entrada_saida(self, operacao, horario):
+        if operacao == Operacao.ENTRADA:
+            self.data_entrada = horario
+        else:
+            self.data_saida = horario
+
+    def remover_duplicatas(alunos):
+
+    def exportar_contatos(arquivo, alunos):
+
 if __name__ == '__main__':
    arquivos_grupos = Grupo.importar_grupos()
    arquivos_grupo = arquivos_grupos[0]
@@ -30,6 +90,6 @@ if __name__ == '__main__':
    pprint(arquivos_grupo)
    grupo = Grupo(arquivos_grupo)
 
-   pprint(f' Grupo: {grupo.nome} \n Número: {grupo.numero}')
+   print(f'Grupo: {grupo.nome} \nNúmero: {grupo.numero}')
 
 
